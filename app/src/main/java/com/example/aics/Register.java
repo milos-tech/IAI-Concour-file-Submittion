17de1239daf96f4b7cbdfb24f98f771a4512e8cd
milosgs <afegenuim@gmail.com>
2024-05-29 19:28:40 +0100

    registration activity

--- app/src/main/java/com/example/aics/Register.java
+++ app/src/main/java/com/example/aics/Register.java
@@ -1,6 +1,9 @@
 package com.example.aics;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
 
 import androidx.activity.EdgeToEdge;
 import androidx.appcompat.app.AppCompatActivity;
@@ -19,6 +22,15 @@ public class Register extends AppCompatActivity {
             Insets systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars());
             v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom);
             return insets;
+        });
+        setContentView(R.layout.activity_register);
+        Button register = findViewById(R.id.submit);
+        register.setOnClickListener(new View.OnClickListener(){
+            @Override
+            public void onClick(View v){
+                Intent intent = new Intent(Register.this, fileUpload.class);
+                startActivity(intent);
+            }
         });
     }
 }
